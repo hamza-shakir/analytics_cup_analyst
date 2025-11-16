82522a9b948c1191239b647c532476f00200b89b
Hamza Shakir <hamzashakir149@gmail.com>
2025-11-16 23:56:06 +0530

    created gamestate package structure

---
+++ src/gamestate/segmentation.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+#
+def segment_by_game_state(match_id):
+    pass
+
+
+#
+def segment_by_goal(match_id):
+    pass
+
+
+#
+def segment_by_time_window(match_id):
+    pass
+
+
+#
+def segment_by_substitution(match_id):
+    pass
